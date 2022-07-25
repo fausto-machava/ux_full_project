121ab5d12aa46499a167ba065c1cf0d17405023b
Faztudo-Machava <fasthymachava12@gmail.com>
2022-07-25 16:39:05 +0200

    Fixing prodution details

--- util/AddUser.php
+++ util/AddUser.php
@@ -13,10 +13,10 @@
         $result->bind_param("sss", $name, $email, $message);
         $result->execute();
         if($result->affected_rows > 0){
-            header('Location: http://'.$_SERVER['HTTP_HOST'].'/contact/index.php?resp=1');
+            header('Location: http://'.$_SERVER['HTTP_HOST'].'/index.php?resp=1');
         } else{
-            header('Location: http://'.$_SERVER['HTTP_HOST'].'/contact/index.php?resp=0');
+            header('Location: http://'.$_SERVER['HTTP_HOST'].'/index.php?resp=0');
         }
     } catch (Exception $e) {
-        header('Location: http://'.$_SERVER['HTTP_HOST'].'/contact/index.php?resp=0&exception='.$e->getMessage());
+        header('Location: http://'.$_SERVER['HTTP_HOST'].'/index.php?resp=0&exception='.$e->getMessage());
     }
