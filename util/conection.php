7d2e025b423d5cbf71f0ec9ac0dd7593c3e347ce
Faztudo-Machava <fasthymachava12@gmail.com>
2022-07-25 16:40:24 +0200

    Fixing prodution details

--- util/conection.php
+++ util/conection.php
@@ -1,10 +1,10 @@
 <?php
     use \PDOException;
 
-	$host = 'localhost';
-	$user = 'root';
-	$password = '1234';
-	$database = 'ux';
+	$host = 'us-cdbr-east-06.cleardb.net';
+	$user = 'b2fb3417b04175';
+	$password = '2be6e974';
+	$database = 'heroku_25155c51c27c72e';
 
     $conection = mysqli_connect($host, $user, $password, $database) or die ('Failed to connect to the database');
 ?>
